c2c1dbe4f0d80132ab92f5936a12406a2496f4e9
caynanr <caynanramos@gmail.com>
2021-08-10 13:04:23 -0300

    Component: error 404

--- src/Helper/NotFound.jsx
+++ src/Helper/NotFound.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styles from "./NotFound.module.css";
+import { ReactComponent as Background } from "../Assets/bg.svg";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Página não encontrada</h1>
+    <div className={styles.background}>
+      <Background />
+      <h1 className={styles.mensagem}>Error: 404 - Página não encontrada.</h1>
+      <Link to="/" className={styles.home}>
+        Home
+      </Link>
     </div>
   );
 };
